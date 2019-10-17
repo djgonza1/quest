b5d0df275d6277e8e4199d9195cdc5d0783699c0
djgonza1 <djgonzalezrev@gmail.com>
2019-10-16 21:45:50 -0700

    Created duel card objects

---
+++ Assets/Silvermine/Scripts/BaseMagicCard.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Silvermine.Duel.Core
+{
+
+    public enum CardColor { None, Red, Green, Blue };
+
+    public class BaseMagicCard
+    {
+        public CardColor Color { get; private set; }
+
+        public int Damage { get; private set; }
+
+        public BaseMagicCard()
+        {
+            Color = CardColor.None;
+            Damage = 0;
+        }
+
+        public BaseMagicCard(CardColor color, int power)
+        {
+            this.Color = color;
+            this.Damage = power;
+        }
+    }
+}
---
+++ Assets/Silvermine/Scripts/CardObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Silvermine.Duel.Core;
+
+public class CardObject : MonoBehaviour
+{
+    [SerializeField]
+    private CardColor _color;
+    [SerializeField]
+    private int _damange;
+
+    private BaseMagicCard _card;
+
+    void Start()
+    {
+        _card = new BaseMagicCard(_color, _damange);
+        Debug.Log("Card is " + _card.Color);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
