d18382d78c706a24cfe6cb0682cd76ba39bf54a4
David Gonzalez <djgonzalezrev@gmail.com>
2019-10-20 01:09:08 -0700

    Created Events

---
+++ Assets/Silvermine/Scripts/Events/Events.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Events
+{
+    private static Events _instance = new Events();
+    public static Events Instance
+    {
+        get { return _instance; }
+    }
+
+    Dictionary<Type, Delegate> _delegates;
+
+    private Events()
+    {
+        if (_instance != null)
+        {
+            return;
+        }
+
+        _instance = this;
+        _delegates = new Dictionary<Type, Delegate>();
+    }
+
+    public delegate void EventDelegate<T>(T e) where T : GameEvent;
+
+    public void AddListener<T>(EventDelegate<T> listener) where T : GameEvent
+    {
+        Type type = typeof(T);
+
+        Delegate d;
+        if (_delegates.TryGetValue(type, out d))
+        {
+            d = Delegate.Remove(d, listener);
+            _delegates[type] = Delegate.Combine(d, listener);
+        }
+        else
+        {
+            _delegates[type] = listener;
+        }
+    }
+
+    public void RemoveListener<T>(EventDelegate<T> listener) where T : GameEvent
+    {
+        Type type = typeof(T);
+
+        Delegate d;
+        if (_delegates.TryGetValue(type, out d))
+        {
+            Delegate existingDel = Delegate.Remove(d, listener);
+
+            if (existingDel == null)
+            {
+                _delegates.Remove(type);
+            }
+            else
+            {
+                _delegates[type] = existingDel;
+            }
+        }
+    }
+
+    public void Raise<T>(T e) where T : GameEvent
+    {
+        if (e == null)
+        {
+            throw new ArgumentNullException("e");
+        }
+
+        Delegate d;
+        if (_delegates.TryGetValue(typeof(T), out d))
+        {
+            if(d is EventDelegate<T> callback)
+            {
+                callback(e);
+            }
+        }
+    }
+}
+
+public class GameEvent { };
+
+public class CardEvent : GameEvent
+{
+    public enum EventType { CARD_PLAYED };
+
+    public EventType type;
+    public CardObject card;
+
+    public CardEvent(EventType type, CardObject card)
+    {
+        this.type = type;
+        this.card = card;
+    }
+}
