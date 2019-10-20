9ef01144fbffd0a9dc1f077dc0bbd3b2d17803a4
djgonza1 <djgonzalezrev@gmail.com>
2019-10-19 19:17:37 -0700

    Player can highligh and grab cards

--- Assets/Silvermine/Scripts/Cards/CardObject.cs
+++ Assets/Silvermine/Scripts/Cards/CardObject.cs
@@ -3,32 +3,120 @@ using Silvermine.Duel.Core;
 
 public class CardObject : MonoBehaviour
 {
+    private enum CardState { None, InHand, Grabbed, Reseting};
+
+    private const float OverSizePosOffset = 1.3f;
+    private const float OverSizeScale = 1.5f;
+
+    [SerializeField]
+    SpriteRenderer _renderer;
     [SerializeField]
     private CardColor _color;
     [SerializeField]
     private int _damange;
 
     private BaseMagicCard _card;
+    private BoardSceneManager _manager;
+    private Vector2 _originalScale;
+    private CardState _state;
 
     void Start()
     {
         _card = new BaseMagicCard(_color, _damange);
-        Debug.Log("Card is " + _card.Color);
+        _manager = BoardSceneManager.Instance;
+        _originalScale = transform.localScale;
+        _state = CardState.InHand;
+        Debug.LogWarning("originalScale " + _originalScale);
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
+        if(_state != CardState.Grabbed)
+        {
+            return;
+        }
+        
+        Vector2 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        this.transform.position = position;
+    }
 
+    private void OnMouseUp()
+    {
+        if(_state == CardState.Grabbed)
+        {
+            _manager.IsHoldingCard = false;
+            ResetCardInHand();
+        }
+        else
+        {
+            GrabCard();
+        }
+    }
+
+    private void OnMouseDrag()
+    {
+        if (_state != CardState.InHand)
+        {
+            return;
+        }
+
+        if (Input.GetAxis("Mouse X") > 0f || Input.GetAxis("Mouse Y") > 0f)
+        {
+            GrabCard();
+        }
     }
 
     private void OnMouseEnter()
     {
-        LeanTween.scale(this.gameObject, new Vector2(1.3f, 1.3f), 0.2f);
+        if (_state != CardState.InHand || _manager.IsHoldingCard)
+        {
+            return;
+        }
+        
+        HighlightCard();
     }
 
     private void OnMouseExit()
     {
-        LeanTween.scale(this.gameObject, new Vector2(1f, 1f), 0.2f);
+        if(_state != CardState.InHand)
+        {
+            return;
+        }
+
+        ResetCardInHand();
+    }
+
+    private void HighlightCard()
+    {
+        Debug.LogWarning("HighlightCard");
+        Vector2 handPosition = _manager.GetCardHandPosition(this);
+
+        LeanTween.scale(this.gameObject, new Vector2(OverSizeScale, OverSizeScale), 0.2f);
+        LeanTween.move(this.gameObject, new Vector2(handPosition.x, handPosition.y + OverSizePosOffset), 0.2f);
+
+        _renderer.sortingOrder++;
+    }
+    
+    private void ResetCardInHand()
+    {
+        _state = CardState.Reseting;
+        Vector2 handPosition = _manager.GetCardHandPosition(this);
+        Vector2 handScale = _manager.GetHandCardScale();
+
+        LeanTween.scale(this.gameObject, handScale, 0.2f);
+        LeanTween.move(this.gameObject, new Vector2(handPosition.x, handPosition.y), 0.2f).setOnComplete(()=> 
+        {
+            _state = CardState.InHand;
+        });
+
+        _renderer.sortingOrder = 0;
+    }
+
+    private void GrabCard()
+    {
+        _state = CardState.Grabbed;
+        _manager.IsHoldingCard = true;
+        Vector2 handScale = _manager.GetHandCardScale();
+        LeanTween.scale(this.gameObject, handScale, 0.2f);
     }
 }
---
+++ Assets/Silvermine/Scripts/Managers/BoardSceneManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Silvermine.Duel.Core;
+
+public class BoardSceneManager : SingletonGameObject<BoardSceneManager>
+{
+    [SerializeField]
+    private CardObject[] _cards;
+    
+    [SerializeField]
+    private Transform[] _cardLocators;
+    
+    private Dictionary<CardObject, Transform> _handMap;
+    public bool IsHoldingCard { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsHoldingCard = false;
+
+        _handMap = new Dictionary<CardObject, Transform>();
+        for(int i = 0; i < _cards.Length; i++)
+        {
+            _handMap.Add(_cards[i], _cardLocators[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Vector2 GetCardHandPosition(CardObject card)
+    {
+        if (_handMap.ContainsKey(card))
+        {
+            return _handMap[card].position;
+        }
+
+        Debug.LogError("No hand locator found for card object");
+        return Vector2.zero;
+    }
+
+    public Vector2 GetHandCardScale()
+    {
+        return new Vector2(0.6f, 0.6f);
+    }
+}
