dadd7e10625b60af5fc7077ce8bc043f783c17ba
djgonza1 <djgonzalezrev@gmail.com>
2019-10-21 00:35:33 -0700

    added Playmaker package

---
+++ Assets/Silvermine/Scripts/Managers/BoardSessionManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Silvermine.Battle.Core
+{
+    public enum Player { First, Second };
+
+    public class BoardSessionManager
+    {
+        public BoardSessionManager()
+        {
+
+        }
+
+        public bool TryPlayCard(Player player, BaseMagicCard card)
+        {
+            return true;
+        }
+    }
+}
