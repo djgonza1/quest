6ceff8e076b8e61ccfe85a713866131f820053f4
djgonza1 <djgonzalezrev@gmail.com>
2019-10-22 00:00:59 -0700

    setup for card objects state machine

---
+++ Assets/Silvermine/Scripts/Playmaker/Cards/DropCard.cs
@@ -0,0 +1,17 @@
+﻿using HutongGames.PlayMaker;
+
+namespace Silvermine.Playmaker.Actions
+{
+    [ActionCategory("Silvermine")]
+    public class DropCard : FsmStateAction
+    {
+        public FsmOwnerDefault Card;
+
+        public override void OnEnter()
+        {
+            var go = Fsm.GetOwnerDefaultTarget(Card);
+
+            go.GetComponent<CardObject>().DropCard(Finish);
+        }
+    }
+}
---
+++ Assets/Silvermine/Scripts/Playmaker/Cards/HighlightCard.cs
@@ -0,0 +1,18 @@
+﻿using HutongGames.PlayMaker;
+
+namespace Silvermine.Playmaker.Actions
+{
+    [ActionCategory("Silvermine")]
+    public class HighlightCard : FsmStateAction
+    {
+        public FsmOwnerDefault Card;
+
+        public override void OnEnter()
+        {
+            var go = Fsm.GetOwnerDefaultTarget(Card);
+
+            go.GetComponent<CardObject>().HighlightCard();
+            Finish();
+        }
+    }
+}
