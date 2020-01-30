7ad9dd4cd94c85139e2d1dcaae8bd60765c4b31f
David Gonzalez <djgonzalezrev@gmail.com>
2020-01-30 00:06:36 -0800

    added start menu scene with vs ai and online search options

---
+++ Assets/Silvermine/Scripts/Managers/StartSceneManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartSceneManager : SingletonGameObject<StartSceneManager>
+{
+    [SerializeField]
+    private Button _vsAIbutton;
+    [SerializeField]
+    private Button _onlineButton;
+    [SerializeField]
+    private Text _onlineText;
+
+    private void Start()
+    {
+        _onlineText.gameObject.SetActive(false);
+    }
+
+    public void OnVsAIButtonClick()
+    {
+        SceneManager.LoadScene("board_scene");
+    }
+
+    public void OnOnlineButtonClick()
+    {
+        _vsAIbutton.gameObject.SetActive(false);
+        _onlineButton.gameObject.SetActive(false);
+
+        _onlineText.gameObject.SetActive(true);
+        _onlineText.text = "Connecting to Server...";
+    }
+}
