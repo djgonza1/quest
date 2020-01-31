b8533672b36d5979876f4d9834be680b5f340ef2
David Gonzalez <djgonzalezrev@gmail.com>
2020-01-31 00:51:45 -0800

    added server support

---
+++ Assets/Silvermine/Scripts/Managers/OnlineBoardSessionManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Silvermine.Battle.Core;
+
+public class OnlineBoardSessionManager
+{
+    private BoardSessionManager _boardSession;
+
+    public OnlineBoardSessionManager(Socket clientOne, Socket clientTwo)
+    {
+        clientOne.BeginSend("hello", SendCallBack);
+    }
+
+    private void SendCallBack(IAsyncResult re)
+    {
+        //TODO - add send logic here
+    }
+}
