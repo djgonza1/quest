687016b0135d2803e608ae8b5b34cf2b33a827cd
David Gonzalez <djgonzalezrev@gmail.com>
2020-10-02 23:24:20 -0700

    Experimenting with including all battle states in BoardSessionManager to reduce file clutter

--- Assets/Silvermine/Scripts/Managers/BoardSessionManager.cs
+++ Assets/Silvermine/Scripts/Managers/BoardSessionManager.cs
@@ -39,5 +39,65 @@ namespace Silvermine.Battle.Core
         {
             StateMachine.Update();
         }
+
+    #region Battle States
+        private class BattleStart : SMState<BoardSessionManager>
+        {
+            public override void Begin()
+            {
+                _context.EventsManager.OnBoardOpen();
+                _stateMachine.ChangeState<ChoosingPhase>();
+            }
+        }
+        
+        private class ChoosingPhase : SMState<BoardSessionManager>
+        {
+            private bool _playerOneCardChosen;
+            private bool _playerTwoCardChosen;
+
+            public override void Begin()
+            {
+                _playerOneCardChosen = false;
+                _playerTwoCardChosen = false;
+
+                _context.EventsManager.OnChoosingPhaseStart();
+
+                _context.PlayerOne.RequestCardChoice(OnPlayerOneCardChosen);
+                _context.PlayerTwo.RequestCardChoice(OnPlayerTwoCardChosen);
+            }
+
+            private void OnPlayerOneCardChosen(AbilityCard cardChoice)
+            {
+                _context.GameBoard.SetPlayerChoice(PlayerType.First, cardChoice);
+                _playerOneCardChosen = true;
+
+                if (_playerOneCardChosen && _playerTwoCardChosen)
+                {
+                    _stateMachine.ChangeState<BattlePhase>();
+                }
+            }
+
+            private void OnPlayerTwoCardChosen(AbilityCard playerTwoChoice)
+            {
+                _context.GameBoard.SetPlayerChoice(PlayerType.Second, playerTwoChoice);
+                _playerTwoCardChosen = true;
+
+                if (_playerOneCardChosen && _playerTwoCardChosen)
+                {
+                    _stateMachine.ChangeState<BattlePhase>();
+                }
+            }
+        }
+
+        private class BattlePhase : SMState<BoardSessionManager>
+        {
+            public override void Begin()
+            {
+                _context.EventsManager.OnBattlePhaseStart(PlayerType.First);
+                _stateMachine.ChangeState<ChoosingPhase>();
+            }
+        }
     }
+    #endregion
+
 }
