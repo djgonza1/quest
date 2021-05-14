acfc723027e76e9887a1f5e26437da105c86bf84
David Gonzalez <djgonzalezrev@gmail.com>
2021-05-13 20:31:02 -0500

    BoardSessionManager removed state machine logic

--- Assets/Silvermine/Scripts/Managers/BoardSessionManager.cs
+++ Assets/Silvermine/Scripts/Managers/BoardSessionManager.cs
@@ -12,7 +12,6 @@ namespace Silvermine.Battle.Core
         public Board GameBoard { get; private set; }
         public IPlayer PlayerOne;
         public IPlayer PlayerTwo;
-        public SMStateMachine<BoardSessionManager> StateMachine { get; private set; }
         public IBattleEventManager EventsManager { get; }
 
         public BoardSessionManager(Board gameBoard, IPlayer playerOne, IPlayer playerTwo, IBattleEventManager eventsManager)
@@ -22,82 +21,5 @@ namespace Silvermine.Battle.Core
             PlayerTwo = playerTwo;
             EventsManager = eventsManager;
         }
-
-        public void StartSession()
-        {
-            SMState<BoardSessionManager>[] states =
-            {
-                new BattleStart(),
-                new ChoosingPhase(),
-                new BattlePhase()
-            };
-
-            StateMachine = new SMStateMachine<BoardSessionManager>(this, states);
-        }
-
-        public void Update()
-        {
-            StateMachine.Update();
-        }
-
-    #region Battle States
-        private class BattleStart : SMState<BoardSessionManager>
-        {
-            public override void Begin()
-            {
-                _context.EventsManager.OnBoardOpen();
-                _stateMachine.ChangeState<ChoosingPhase>();
-            }
-        }
-        
-        private class ChoosingPhase : SMState<BoardSessionManager>
-        {
-            private bool _playerOneCardChosen;
-            private bool _playerTwoCardChosen;
-
-            public override void Begin()
-            {
-                _playerOneCardChosen = false;
-                _playerTwoCardChosen = false;
-
-                _context.EventsManager.OnChoosingPhaseStart();
-
-                _context.PlayerOne.RequestCardChoice(OnPlayerOneCardChosen);
-                _context.PlayerTwo.RequestCardChoice(OnPlayerTwoCardChosen);
-            }
-
-            private void OnPlayerOneCardChosen(AbilityCard cardChoice)
-            {
-                _context.GameBoard.SetPlayerChoice(PlayerType.First, cardChoice);
-                _playerOneCardChosen = true;
-
-                if (_playerOneCardChosen && _playerTwoCardChosen)
-                {
-                    _stateMachine.ChangeState<BattlePhase>();
-                }
-            }
-
-            private void OnPlayerTwoCardChosen(AbilityCard playerTwoChoice)
-            {
-                _context.GameBoard.SetPlayerChoice(PlayerType.Second, playerTwoChoice);
-                _playerTwoCardChosen = true;
-
-                if (_playerOneCardChosen && _playerTwoCardChosen)
-                {
-                    _stateMachine.ChangeState<BattlePhase>();
-                }
-            }
-        }
-
-        private class BattlePhase : SMState<BoardSessionManager>
-        {
-            public override void Begin()
-            {
-                _context.EventsManager.OnBattlePhaseStart(PlayerType.First);
-                _stateMachine.ChangeState<ChoosingPhase>();
-            }
-        }
     }
-    #endregion
-
 }
