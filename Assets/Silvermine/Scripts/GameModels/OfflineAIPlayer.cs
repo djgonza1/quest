eaf5ee4625583f9791a46379a86fc1df0b5fa8e7
David Gonzalez <djgonzalezrev@gmail.com>
2021-06-09 18:21:45 -0500

    comment out AI logic for now. Not used.

--- Assets/Silvermine/Scripts/GameModels/OfflineAIPlayer.cs
+++ Assets/Silvermine/Scripts/GameModels/OfflineAIPlayer.cs
@@ -5,27 +5,27 @@ using UnityEngine;
 
 namespace Silvermine.Battle.Core
 {
-    public class OfflineAIPlayer : IPlayer
-    {
-        public PlayerInfo Info { get => _aiPlayer.Info; }
-        private AIPlayer _aiPlayer;
-        private BoardSceneManager _sceneManager;
+    // public class OfflineAIPlayer : IPlayer
+    // {
+    //     public PlayerInfo Info { get => _aiPlayer.Info; }
+    //     private AIPlayer _aiPlayer;
+    //     private BoardSceneManager _sceneManager;
 
-        public OfflineAIPlayer(Board board, PlayerInfo info, BoardSceneManager sceneManager)
-        {
-            _aiPlayer = new AIPlayer(board, info);
-            _sceneManager = sceneManager;
-        }
+    //     public OfflineAIPlayer(Board board, PlayerInfo info, BoardSceneManager sceneManager)
+    //     {
+    //         _aiPlayer = new AIPlayer(board, info);
+    //         _sceneManager = sceneManager;
+    //     }
 
-        public void RequestCardChoice(Action<AbilityCard> onCardChosen)
-        {
-            AbilityCard cardChoice = _aiPlayer.ChooseCardToPlay();
+    //     public void RequestCardChoice(Action<AbilityCard> onCardChosen)
+    //     {
+    //         AbilityCard cardChoice = _aiPlayer.ChooseCardToPlay();
 
-            _sceneManager.PlayerTwoChoice = _sceneManager.EnemyHandController.GetCard(cardChoice);
-            onCardChosen(cardChoice);
+    //         _sceneManager.PlayerTwoChoice = _sceneManager.EnemyHandController.GetCard(cardChoice);
+    //         onCardChosen(cardChoice);
 
-            PlayableCardBehaviour enemyGO = _sceneManager.EnemyHandController.GetCard(cardChoice);
-            _sceneManager.EnemyHandController.PlayCard(enemyGO);
-        }
-    }
+    //         PlayableCardBehaviour enemyGO = _sceneManager.EnemyHandController.GetCard(cardChoice);
+    //         _sceneManager.EnemyHandController.PlayCard(enemyGO);
+    //     }
+    // }
 }
