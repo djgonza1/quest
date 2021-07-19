d2c1f8704bcf579ad836c4b36eecd6111646b1a1
David Gonzalez <djgonzalezrev@gmail.com>
2021-07-18 23:38:58 -0500

    added CardFocusHolder

---
+++ Assets/Silvermine/Scripts/Controllers/CardFocusHolder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardFocusHolder : MonoBehaviour
+{
+    [SerializeField] public Transform FocusCardLocator;
+    [SerializeField] PlayableCardBehaviour _focusedCard;
+
+    public IEnumerator FocusCardAndChoseTargets(Action<PlayableCardBehaviour> callback = null)
+    {
+        yield return 0;
+
+        callback?.Invoke(null);
+    }
+}
