25d0c91f64a95848f67e73b9f7f31b1bf396e75b
David Gonzalez <djgonzalezrev@gmail.com>
2021-07-18 23:43:01 -0500

    Removed unecessary logic from PlayerBehavior

--- Assets/Silvermine/Scripts/Managers/PlayerBehavior.cs
+++ Assets/Silvermine/Scripts/Managers/PlayerBehavior.cs
@@ -10,6 +10,7 @@ public abstract class PlayerBehavior : MonoBehaviour
     public CardHandController HandController => _playerHand;
 
     public PlayerInfo Info { get; private set; }
+    public abstract PlayableCardBehaviour CardChoice { get; }
 
     public void Init(PlayerInfo info, PlayerType playerType = PlayerType.First)
     {
@@ -17,13 +18,6 @@ public abstract class PlayerBehavior : MonoBehaviour
         _playerHand.Init(Info.Hand, playerType);
     }
 
-    public IEnumerator PlayCard(Action<PlayableCardBehaviour> callback = null)
-    {
-        yield return WaitForCardPlayed();
-        Debug.LogWarning("CardChosen");
-        callback(CardChoice);
-    }
+    public abstract IEnumerator PlayCard(Action<PlayableCardBehaviour> callback = null);
 
-    public abstract PlayableCardBehaviour CardChoice { get; }
-    protected abstract IEnumerator WaitForCardPlayed();
 }
