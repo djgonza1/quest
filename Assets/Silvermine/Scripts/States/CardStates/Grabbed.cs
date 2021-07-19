e8ee3a6ebb9156089e9ba214840a873715d32524
David Gonzalez <djgonzalezrev@gmail.com>
2021-07-18 23:39:23 -0500

    Comment out Grabbed state, not used at the moment

--- Assets/Silvermine/Scripts/States/CardStates/Grabbed.cs
+++ Assets/Silvermine/Scripts/States/CardStates/Grabbed.cs
@@ -4,59 +4,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using Silvermine.Battle.Core;
 
-public class Grabbed : SMState<PlayableCardBehaviour>
-{
-    private CardHandController _handController;
-    private bool _keepGrabbing;
+// public class Grabbed : SMState<PlayableCardBehaviour>
+// {
+//     private CardHandController _handController;
+//     private bool _keepGrabbing;
 
-    public Grabbed(CardHandController handController)
-    {
-        _handController = handController;
-    }
+//     public Grabbed(CardHandController handController)
+//     {
+//         _handController = handController;
+//     }
 
-    public override void Begin()
-    {
-        _keepGrabbing = true;
+//     public override void Begin()
+//     {
+//         _keepGrabbing = true;
 
-        _context.OnMouseUnclicksCard += OnCardTapRelease;
+//         _context.OnMouseUnclicksCard += OnCardTapRelease;
         
-        _handController.OnCardGrabbed(_context);
+//         _handController.OnCardGrabbed(_context);
 
-        Vector2 handScale = _handController.GetHandCardScale();
-        LeanTween.scale(_context.gameObject, handScale, 0.2f);
-    }
+//         Vector2 handScale = _handController.GetHandCardScale();
+//         LeanTween.scale(_context.gameObject, handScale, 0.2f);
+//     }
 
-    public override void End()
-    {
-        _context.OnMouseUnclicksCard -= OnCardTapRelease;
-    }
+//     public override void End()
+//     {
+//         _context.OnMouseUnclicksCard -= OnCardTapRelease;
+//     }
 
-    public override IEnumerator Reason()
-    {
-        while (_keepGrabbing)
-        {
-            _context.transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            yield return 0;
-        }
-    }
+//     public override IEnumerator Reason()
+//     {
+//         while (_keepGrabbing)
+//         {
+//             _context.transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+//             yield return 0;
+//         }
+//     }
 
-    private void OnCardTapRelease(PlayableCardBehaviour card)
-    {
-        _keepGrabbing = false;
-        RaycastHit2D[] hits = Physics2D.RaycastAll(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.forward);
+//     private void OnCardTapRelease(PlayableCardBehaviour card)
+//     {
+//         _keepGrabbing = false;
+//         RaycastHit2D[] hits = Physics2D.RaycastAll(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.forward);
 
-        foreach (var hit in hits)
-        {
-            if (hit.collider.tag == "PlaySpace")
-            {
-                _handController.PlayCard(card);
-                return;
-            }
-        }
+//         foreach (var hit in hits)
+//         {
+//             if (hit.collider.tag == "PlaySpace")
+//             {
+//                 _handController.PlayCard(card);
+//                 return;
+//             }
+//         }
 
-        _handController.ResetCardInHand(_context, () =>
-        {
-            _stateMachine.ChangeState<ChoosableInHand>();
-        });
-    }
-}
+//         _handController.ResetCardInHand(_context, () =>
+//         {
+//             _stateMachine.ChangeState<ChoosableInHand>();
+//         });
+//     }
+// }
